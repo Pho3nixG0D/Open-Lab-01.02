d1a28c49de6d08af4b4fa78367303bc67567dcad
Oliver Krška <oliverkrska@gmail.com>
2022-09-17 17:02:08 +0200

    Update Exercise.cs

--- Open-Lab-01.02/Exercise.cs
+++ Open-Lab-01.02/Exercise.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_01._02
 {
@@ -6,6 +6,7 @@ namespace Open_Lab_01._02
     {
         public int Sum(int a, int b)
         {
+            return a + b;
             throw new NotImplementedException();
         }
     }
